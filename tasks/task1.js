da0217d6e8e664e5c86a0cb24e64e93ee1911f94
Rodrigo Cespedes <rodri.tronick@gmail.com>
2015-11-16 14:49:04 -0400

    first task with recursive functions.
    I did the task 1

--- tasks/task1.js
+++ tasks/task1.js
@@ -1,4 +1,127 @@
 /**
  * Created by rodrigocespedes on 11/16/2015.
  */
-asdsd
+
+console.log("Evaluate: ", evaluate(15,8,3,4,5,6))
+
+function evaluate(a, b, c)
+{
+    console.log('Arguments: ', arguments);
+
+    var su = sum(arguments, arguments.length-1);
+    var ma = max(0, arguments, arguments.length-1);
+    var mi = min(9999999, arguments, arguments.length-1);
+    console.log('sum ', su);
+    console.log('max ', ma);
+    console.log('min ', mi);
+    // console.log('ave ', );
+
+}
+
+function sum(arg, index)
+{
+    if(index <= 0)
+    {
+        if(index < 0)
+        {
+            return "Dont exist arguments";
+        }
+        else
+            return arg[index];
+    }
+    else
+    {
+        return arg[index] + sum(arg, index-1);
+    }
+}
+
+function max(maxArgument, arguments, index)
+{
+    if(index <= 0)
+    {
+        if(index < 0)
+        {
+            return "Dont exist arguments";
+        }
+        else
+        {
+            if (arguments[index] > maxArgument)
+            {
+                maxArgument = arguments[index]
+            }
+        }
+        return maxArgument;
+    }
+    else
+    {
+        if(arguments[index] > maxArgument)
+        {
+            maxArgument = arguments[index]
+        }
+        index = index -1;
+        return max(maxArgument, arguments, index);
+    }
+}
+
+function min(minArgument, arguments, index)
+{
+    if(index <= 0)
+    {
+        if(index < 0)
+        {
+            return "Dont exist arguments";
+        }
+        else
+        {
+            if (arguments[index] < minArgument)
+            {
+                minArgument = arguments[index]
+            }
+        }
+        return minArgument;
+    }
+    else
+    {
+        if(arguments[index] < minArgument)
+        {
+            minArgument = arguments[index]
+        }
+        index = index -1;
+        return min(minArgument, arguments, index);
+    }
+}
+
+function avg(arg, index)
+{
+    if(index <= 0)
+    {
+        if(index < 0)
+        {
+            return "Dont exist arguments";
+        }
+        else
+            return arg[index];
+    }
+    else
+    {
+        return arg[index] + sum(arg, index-1);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
